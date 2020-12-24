c23716c8d5c08478bf86b2f70cbcf2172877d6a6
wahyu <wahyuizzudin20@gmail.com>
2020-12-24 22:15:58 +0700

    ubah form nama ke atasnya email saat register

--- src/ui/projects/buat.js
+++ src/ui/projects/buat.js
@@ -13,7 +13,7 @@ class ProjectBuat extends React.Component{
                 <div class="container">
                     <div class="row mb-2">
                     <div class="col-sm-6">
-                        <h1 class="m-0"> Welcome to <b>PlayProjects</b></h1>
+                        <h1 class="m-0"> Welcome back <b>PlayProjects</b></h1>
                     </div>
                     <div class="col-sm-6">
                         <ol class="breadcrumb float-sm-right">
